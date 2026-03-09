080a27a2692f34e103eb8f0f2dca454752d5fa0e
Tri Aldy Kurniawan <122498697+trialdyk@users.noreply.github.com>
2026-03-09 09:14:19 +0700

    Pertemuan 4 - Praktikum 2.6

--- pertemuan-4/routes/web.php
+++ pertemuan-4/routes/web.php
@@ -8,3 +8,8 @@ Route::get('/', function () {
 });
 
 Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+Route::post('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
