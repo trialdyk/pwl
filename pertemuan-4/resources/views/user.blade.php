382f757ceccd1c01c8707f50a9f8316c8f14883a
Tri Aldy Kurniawan <122498697+trialdyk@users.noreply.github.com>
2026-03-09 08:43:06 +0700

    Pertemuan 4 - Praktikum 2

---
+++ pertemuan-4/resources/views/user.blade.php
@@ -0,0 +1,17 @@
+<body>
+    <h1>Data User</h1>
+    <table border="1" cellpadding="2" cellspacing="0">
+        <tr>
+            <td>ID</td>
+            <td>Username</td>
+            <td>Nama</td>
+            <td>ID Level Pengguna</td>
+        </tr>
+        <tr>
+            <td>{{ $data->user_id }}</td>
+            <td>{{ $data->username }}</td>
+            <td>{{ $data->nama }}</td>
+            <td>{{ $data->level_id }}</td>
+        </tr>
+    </table>
+</body>
