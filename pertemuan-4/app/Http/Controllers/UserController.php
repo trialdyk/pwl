f6e011a20f1d27fc23cbccf8e46fbc746993b24a
Tri Aldy Kurniawan <122498697+trialdyk@users.noreply.github.com>
2026-03-09 08:46:08 +0700

    Pertemuan 4 - Praktikum 2.2

--- pertemuan-4/app/Http/Controllers/UserController.php
+++ pertemuan-4/app/Http/Controllers/UserController.php
@@ -10,10 +10,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::findOr(20, ['username', 'nama'], function () {
-            abort(404);
-        });
-        
+        $user = UserModel::where('username', 'manager3')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
