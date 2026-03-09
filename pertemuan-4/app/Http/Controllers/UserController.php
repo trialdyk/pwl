738a0a12210aa4abb7fad9b4dbab7513e681403a
Tri Aldy Kurniawan <122498697+trialdyk@users.noreply.github.com>
2026-03-09 08:30:33 +0700

    Pertemuan 4 - Praktikum 1

---
+++ pertemuan-4/app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $data = [
+            'level_id' => 3,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
+        ];
+        UserModel::create($data);
+
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }
+}
---
+++ pertemuan-4/app/Models/UserModel.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class UserModel extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['level_id', 'username', 'nama'];
+}
