82ad4e717780e16b5f436ea7a4465a9704ed4b80
Tri Aldy Kurniawan <122498697+trialdyk@users.noreply.github.com>
2026-04-23 20:38:14 +0700

    Jobsheet 6 - Pertemuan 5 - Tugas Praktikum

--- pertemuan-5/app/Filament/Resources/Posts/Schemas/PostForm.php
+++ pertemuan-5/app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -11,28 +11,49 @@ use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\DatePicker;
+use Filament\Schemas\Components\Group;
+use Filament\Schemas\Components\Section;
+
 class PostForm
 {
     public static function configure(Schema $schema): Schema
     {
         return $schema
             ->components([
-                TextInput::make('title')->required()->minLength(5),
-                TextInput::make('slug')->required()->unique(ignoreRecord: true),
-                Select::make('category_id')
-                        ->label('Category')
-                        ->options(
-                            \App\Models\Category::all()->pluck('name', 'id')
-                        )
-                        ->required(),
-                ColorPicker::make('color'),
-                MarkdownEditor::make('body'),
-                FileUpload::make('image')
-                    ->disk('public')
-                    ->directory('post'),
-                TagsInput::make('tags'),
-                Checkbox::make('published'),
-                DatePicker::make('published_at'),
-            ]);
+                Group::make()->schema([
+                    Section::make('Main Content')
+                        ->icon('heroicon-o-document-text')
+                        ->schema([
+                            TextInput::make('title')->required()->minLength(5),
+                            TextInput::make('slug')->required()->unique(ignoreRecord: true),
+                            MarkdownEditor::make('body')->columnSpanFull(),
+                        ])->columns(2),
+
+                    Section::make('Media')
+                        ->icon('heroicon-o-photo')
+                        ->schema([
+                            FileUpload::make('image')
+                                ->disk('public')
+                                ->directory('post'),
+                        ])
+                ])->columnSpan(2),
+
+                Group::make()->schema([
+                    Section::make('Meta')
+                        ->icon('heroicon-o-tag')
+                        ->schema([
+                            Select::make('category_id')
+                                ->label('Category')
+                                ->options(
+                                    \App\Models\Category::all()->pluck('name', 'id')
+                                )
+                                ->required(),
+                            ColorPicker::make('color'),
+                            TagsInput::make('tags'),
+                            Checkbox::make('published'),
+                            DatePicker::make('published_at'),
+                        ])
+                ])->columnSpan(1),
+            ])->columns(3);
     }
 }
