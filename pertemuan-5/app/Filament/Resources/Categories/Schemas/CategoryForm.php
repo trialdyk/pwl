c3b0e8f59033e48abd2b1fca864a52c929a91075
Tri Aldy Kurniawan <122498697+trialdyk@users.noreply.github.com>
2026-04-23 20:07:30 +0700

    Jobsheet 5 - Pertemuan 3 - Tugas Praktikum

---
+++ pertemuan-5/app/Filament/Resources/Categories/Schemas/CategoryForm.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Resources\Categories\Schemas;
+
+use Filament\Schemas\Schema;
+use Filament\Forms\Components\TextInput;
+
+class CategoryForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                ->required(),
+                TextInput::make('slug')
+                ->required()
+                ->unique(ignoreRecord: true),
+            ]);
+    }
+}
